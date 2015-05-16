f2c958dfef544806e99cffe49a15f81f77af1ad5
Codi Burley <idocyelrub@gmail.com>
2015-05-16 08:58:55 -0400

    added body parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var mongoose = require('mongoose');
 var uriUtil = require('mongodb-uri');
 var request = require('request');
+var bodyParser = require('body-parser');
 
 var PartyModel = require('./models/Party').Party,
 	SongModel = require('./models/Party').Song;
@@ -12,6 +13,10 @@ var mongoURI = "mongodb://dualranger:hack2015@ds031892.mongolab.com:31892/jukebo
 	mongooseURI = uriUtil.formatMongoose(mongoURI);
 mongoose.connect(mongooseURI);
 
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //Set up socket.io
 var io = require('socket.io')(http);
 io.on('connection', function(socket) {
@@ -19,13 +24,10 @@ io.on('connection', function(socket) {
 });
 
 app.post('/userauth', function(req, res, next) {
-	//var user_id = req.body.user_id,
-	//	auth_key = req.body.token;
-	for(var key in req) {
-		console.log('Key: ' + key);
-	}
-	//console.log("USER ID:" + user_id);
-	//console.log("AUTH KEY" + auth_key);
+	var user_id = req.body.user_id,
+		auth_key = req.body.token;
+	console.log("USER ID:" + user_id);
+	console.log("AUTH KEY" + auth_key);
 	//set up request to get playlist from spotify
 	/*var _headers = {
 		'Authentication': 'Bearer' + auth_key
