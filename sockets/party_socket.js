71178b777793d201b90df21e5e544abcb9701b76
Codi Burley <idocyelrub@gmail.com>
2015-06-01 17:08:33 -0400

    debugging

--- sockets/party_socket.js
+++ sockets/party_socket.js
@@ -17,37 +17,37 @@ var io = null,
 
 module.exports = {
 
-	hostParty: function(res) {
-		console.log('PLAYLIST BEING REQUESTED');
-		var user_id = res.user_id,
-			auth_key = res.token;
-		PartyModel.findOne({ 'name' : res.party }, function(err, results) {
-			if(results) { return socket.emit('party-name-taken'); } // if party exists
-			//set up request to get playlist from spotify
-			var options = {
-				url: 'https://api.spotify.com/v1/users/'+ user_id +'/playlists/' + res.list_id + '/tracks' ,
-				method: 'GET',
-				headers: { 'Authorization': 'Bearer ' + auth_key }
-			};		
-			request(options, function(err, resp, body) {
-				if(!err) {
-					body = JSON.parse(body);
-					var tracks = parseTracks(body.items),
-					    uriArray = getURIs(tracks);
-					socket.join(res.party);
-					var newParty = new PartyModel({
-						name: res.party,
-						queue: uriArray,
-						timestamp: Util.stampTime()
-					});
-					newParty.save(function(err) {
-						if(err) { return console.log(err); }
-						else { return saveSongs(tracks, newParty.id, res.party, tracks.length - 1); }
-					});
-				}else { console.log(err); }
-			});
-		});
-	},
+	// hostParty: function(res) {
+	// 	console.log('PLAYLIST BEING REQUESTED');
+	// 	var user_id = res.user_id,
+	// 		auth_key = res.token;
+	// 	PartyModel.findOne({ 'name' : res.party }, function(err, results) {
+	// 		if(results) { return socket.emit('party-name-taken'); } // if party exists
+	// 		//set up request to get playlist from spotify
+	// 		var options = {
+	// 			url: 'https://api.spotify.com/v1/users/'+ user_id +'/playlists/' + res.list_id + '/tracks' ,
+	// 			method: 'GET',
+	// 			headers: { 'Authorization': 'Bearer ' + auth_key }
+	// 		};		
+	// 		request(options, function(err, resp, body) {
+	// 			if(!err) {
+	// 				body = JSON.parse(body);
+	// 				var tracks = parseTracks(body.items),
+	// 				    uriArray = getURIs(tracks);
+	// 				socket.join(res.party);
+	// 				var newParty = new PartyModel({
+	// 					name: res.party,
+	// 					queue: uriArray,
+	// 					timestamp: Util.stampTime()
+	// 				});
+	// 				newParty.save(function(err) {
+	// 					if(err) { return console.log(err); }
+	// 					else { return saveSongs(tracks, newParty.id, res.party, tracks.length - 1); }
+	// 				});
+	// 			}else { console.log(err); }
+	// 		});
+	// 	});
+	// },
 
 	hostParty: function(res) {
 		console.log('PLAYLIST BEING REQUESTED');
