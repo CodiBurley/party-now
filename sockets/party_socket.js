d9d819d28e96690fc1127e376abbfb5cf1337381
Codi Burley <idocyelrub@gmail.com>
2015-06-01 16:32:16 -0400

    debugging

--- sockets/party_socket.js
+++ sockets/party_socket.js
@@ -54,7 +54,7 @@ module.exports = {
 		var user_id = res.user_id,
 			auth_key = res.token;
 		//PartyModel.findOne({ 'name' : res.party }, function(err, results) {
-			console.log(Disposal(res.party));
+			console.log('DISPOSAL: ' + Disposal(res.party));
 			if(!Disposal(res.party)) { 
 				console.log('NAME TAKEN');
 				return socket.emit('party-name-taken'); 
